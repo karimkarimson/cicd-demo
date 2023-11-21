28345b5a783bf787e12c57077cd6476f05ce467b
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-11-21 12:56:45 +0100

    pipe

--- infra/modules/s3-website/main.tf
+++ infra/modules/s3-website/main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "bucket" {
   backend = "s3"
   config = {
     bucket = var.bucket_name_backup
-    key    = "/terraform/state"
+    key    = "terraform/state"
     region = var.region
   }
 }
