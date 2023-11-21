74b3a77bc79bff5e06239b9ce0260070d52f4392
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-11-21 10:14:26 +0100

    pipeline test commit

---
+++ infra/outputs.tf
@@ -0,0 +1,19 @@
+output "bucket_arn" {
+  value = module.s3-website.bucket_arn
+}
+
+output "bucket_domain_name" {
+  value = module.s3-website.bucket_domain_name
+}
+
+output "bucket_id" {
+  value = module.s3-website.bucket_id
+}
+
+output "website_endpoint" {
+  value = module.s3-website.website_endpoint
+}
+
+output "website_domain" {
+  value = module.s3-website.website_domain
+}
