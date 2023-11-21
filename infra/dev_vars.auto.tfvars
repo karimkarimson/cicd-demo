4d8590afafe515635a550fcf6c0a383fd7ea8cb2
Karim Aouini <karim.aouini@docc.techstarter.de>
2023-11-21 09:40:56 +0100

    init commit ci-cd github actions

---
+++ infra/dev_vars.auto.tfvars
@@ -0,0 +1,3 @@
+region      = "eu-central-1"
+aws_profile = "techstarte"
+bucket_name = "cicd-bucket-k1m0-738135"
---
+++ infra/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "aws_profile" {
+  type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
